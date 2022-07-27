521f1e2bc41867f8cda35effed927bbe2a6eb881
Pilag6 <pilag6@gmail.com>
2022-07-27 13:40:05 +0200

    Slider

--- js/main.js
+++ js/main.js
@@ -1,14 +1,79 @@
-// Change Nv Style on Scroll
+// Right Clik Desactivado
 
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
-})
+document.oncontextmenu = function(){return false}
+
+// Change Nav Style on Scroll
+
+window.addEventListener("scroll", () => {
+    document
+        .querySelector("nav")
+        .classList.toggle("window-scrolled", window.scrollY > 0);
+});
 
 // Contact buttons (circular text buttons)
 
-const textButtons = document.querySelectorAll('.contact__btn');
+const textButtons = document.querySelectorAll(".contact__btn");
 
-textButtons.forEach(textButtons => {
-    let text = textButtons.querySelector('p');
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
-})
+textButtons.forEach((textButtons) => {
+    let text = textButtons.querySelector("p");
+    text.innerHTML = text.innerHTML
+        .split("")
+        .map(
+            (character, index) =>
+                `<span style="transform: rotate(${
+                    index * 12
+                }deg)">${character}</span>`
+        )
+        .join("");
+});
+
+//Swipper JS
+var swiper = new Swiper(".mySwiper", {
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
+    mousewheel: {
+    invert: true,
+  },
+
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+
+        1535: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+
+        1919: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+
+        2159: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+    },
+});
+
+swiper.autoplay.start();

c9878607a9d57a246932c2a0661dc7cc9b94c1d6
Pilag6 <pilag6@gmail.com>
2022-07-28 12:14:52 +0200

    Hero and About Responsive

--- js/main.js
+++ js/main.js
@@ -10,6 +10,32 @@ window.addEventListener("scroll", () => {
         .classList.toggle("window-scrolled", window.scrollY > 0);
 });
 
+// Responsive NAVBAR
+
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+}
+
+openNavBtn.addEventListener('click', openNav)
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+})
+
 // Contact buttons (circular text buttons)
 
 const textButtons = document.querySelectorAll(".contact__btn");
